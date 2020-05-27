9691b533ba77e253b4d6c647610b818366f69a08
krishna <krishna.fnatic@gmail.com>
2020-05-27 10:55:12 +0530

    teset

--- resources/views/c1.blade.php
+++ resources/views/c1.blade.php
@@ -32,7 +32,7 @@ c1
     var channel = pusher.subscribe('private-stream-channel');
     channel.bind('notify-patient', function(data) {
       // alert(JSON.stringify(data));
-      console.log(data)
+      console.log(data.data)
       peer2.signal(data)
       
     });
