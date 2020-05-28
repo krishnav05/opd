41aa5a393b1d5290d56125990e9d211a5448f784
krishna <krishna.fnatic@gmail.com>
2020-05-28 21:58:59 +0530

    foix

--- resources/views/video_call.blade.php
+++ resources/views/video_call.blade.php
@@ -5,8 +5,8 @@
 
       <iframe
         src="https://tokbox.com/embed/embed/ot-embed.js?embedId=2801050d-72da-4caa-86fd-367153da06e1&room=DEFAULT_ROOM&iframe=true"
-        width="800px"
-        height="640px"
+        width="100%"
+        height="100%"
         scrolling="auto"
         allow="microphone; camera"
       ></iframe>
