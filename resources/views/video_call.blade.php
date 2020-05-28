8b9e5e438f1a1ef7b7cd48ea3fffc5dbffccf79b
krishna <krishna.fnatic@gmail.com>
2020-05-28 22:13:56 +0530

    fic

--- resources/views/video_call.blade.php
+++ resources/views/video_call.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-      <div id="otEmbedContainer" style="width:100%; height:100%;position: absolute;"></div> <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=2801050d-72da-4caa-86fd-367153da06e1&room=DEFAULT_ROOM"></script>
+      <div id="otEmbedContainer" style="width:100%; height:100%;position: absolute;overflow: hidden;"></div> <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=2801050d-72da-4caa-86fd-367153da06e1&room=DEFAULT_ROOM"></script>
     
 
 @endsection
