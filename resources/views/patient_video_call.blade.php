b2e58a5eaa8b555b83c2f60c5c6750de46a0e358
krishna <krishna.fnatic@gmail.com>
2020-06-01 17:45:08 +0530

    fix

--- resources/views/patient_video_call.blade.php
+++ resources/views/patient_video_call.blade.php
@@ -1,7 +1,55 @@
-<html>
-<head>
-    <title> OnlyOPD Video Call </title>
-    <style>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Basic Video Chat</title>
+    <style type="text/css">
+      html {
+  box-sizing: border-box;
+  height: 100%;
+}
+ 
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+  margin: 0;
+  padding: 0;
+}
+ 
+body {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+ 
+header {
+  text-align: center;
+  padding: 0.5em;
+}
+ 
+main {
+  flex: 1;
+  display: flex;
+  position: relative;
+}
+ 
+input,
+button {
+  font-size: inherit;
+  padding: 0.5em;
+}
+ 
+.registration {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+}
+ 
+.registration input[type="text"] {
+  display: block;
+  margin-bottom: 1em;
+}
+ 
 .subscriber {
   width: 100%;
   height: 100%;
@@ -17,13 +65,30 @@
   align-self: flex-end;
 }
     </style>
-    <script src="https://static.opentok.com/v2/js/opentok.js"></script>
-</head>
-<body>
-    <!-- <main> -->
+    <meta name="description" content="A basic audio-video chat application" />
+    <link
+      id="favicon"
+      rel="icon"
+      href="https://tokbox.com/developer/favicon.ico"
+      type="image/x-icon"
+    />
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+ 
+    <!-- <link rel="stylesheet" href="/style.css" /> -->
+  </head>
+ 
+  <body>
+    <header>
+      <h1>The most basic video chat</h1>
+    </header>
+ 
+    <main>
       <div id="subscriber" class="subscriber"></div>
       <div id="publisher" class="publisher"></div>
-    <!-- </main> -->
+    </main>
+ 
 
     <!-- <script src="https://static.opentok.com/v2/js/opentok.min.js"></script> -->
     <script type="text/javascript">
