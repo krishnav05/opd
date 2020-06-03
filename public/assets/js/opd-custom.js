a56e00f2b7817e35e3d14e42c3fac3b352cb37ec
krishna <krishna.fnatic@gmail.com>
2020-06-03 20:03:39 +0530

    fix

--- public/assets/js/opd-custom.js
+++ public/assets/js/opd-custom.js
@@ -23,43 +23,19 @@ const COLOR_CODES = {
   }
 };
 
-const TIME_LIMIT = 20;
+const TIME_LIMIT = 180;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
 let remainingPathColor = COLOR_CODES.info.color;
 
-// document.getElementById("app").innerHTML = `
-// <div class="base-timer">
-//   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-//     <g class="base-timer__circle">
-//       <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-//       <path
-//         id="base-timer-path-remaining"
-//         stroke-dasharray="283"
-//         class="base-timer__path-remaining ${remainingPathColor}"
-//         d="
-//           M 50, 50
-//           m -45, 0
-//           a 45,45 0 1,0 90,0
-//           a 45,45 0 1,0 -90,0
-//         "
-//       ></path>
-//     </g>
-//   </svg>
-//   <span id="base-timer-label" class="base-timer__label">${formatTime(
-//     timeLeft
-//   )}</span>
-// </div>
-// `;
-
-// startTimer();
-
 function onTimesUp() {
   clearInterval(timerInterval);
-  $('#divs').css('display','none');
-  $('#app').html('');
-  $('#findnow').show();
+  document.getElementById("app").innerHTML = `<div style="text-align: center;">It's taking more time than usual,<br>
+  <strong>Doctors</strong> are busy with <strong>other patients.</strong><br>Please give us few more minutes.</div>`;
+  // $('#divs').css('display','none');
+  // $('#app').html('');
+  // $('#findnow').show();
 }
 
 function startTimer() {
@@ -164,30 +140,141 @@ function setCircleDasharray() {
     </div>
     `;
     startTimer();
-    // setTimeout(function() {
-    //   $('#find-doc').modal();
-    // }, 20000);
+
+ setInterval(resend, 190000);
   });
-//Doc waiting time
-// Credit: Mateusz Rybczonec
+  function resend()
+  {
+    const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "green",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "green",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+const TIME_LIMIT = 180;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+  document.getElementById("app").innerHTML = `<div style="text-align: center;">It's taking more time than usual,<br>
+  <strong>Doctors</strong> are busy with <strong>other patients.</strong><br>Please give us few more minutes.</div>`;
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
 
 
 
-//code for wait time
 
-// var startTime, endTime;
+    var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+    $.ajax({
+                    /* the route pointing to the post function */
+                    url: "find-doc",
+                    type: 'POST',
+                    /* send the csrf-token and the input to the controller */
+                    data: {_token: CSRF_TOKEN},
+                    dataType: 'JSON',
+                    /* remind that 'data' is the response of the AjaxController */
+                    success: function (data) { 
+                       window.userid = data.id;
+                       // alert(window.userid);
+                    }
+                });
 
-// function start() {
-//   startTime = performance.now();
-// };
-
-// function end() {
-//   endTime = performance.now();
-//   var timeDiff = endTime - startTime; //in ms 
-//   // strip the ms 
-//   timeDiff /= 1000; 
-  
-//   // get seconds 
-//   var seconds = Math.round(timeDiff);
-//   console.log(seconds + " seconds");
-// }
+    document.getElementById("app").innerHTML = `
+    <div class="base-timer">
+    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+    <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+    <path
+    id="base-timer-path-remaining"
+    stroke-dasharray="283"
+    class="base-timer__path-remaining ${remainingPathColor}"
+    d="
+    M 50, 50
+    m -45, 0
+    a 45,45 0 1,0 90,0
+    a 45,45 0 1,0 -90,0
+    "
+    ></path>
+    </g>
+    </svg>
+    <span id="base-timer-label" class="base-timer__label">${formatTime(
+      timeLeft
+      )}</span>
+    </div>
+    `;
+    startTimer();
+  }
