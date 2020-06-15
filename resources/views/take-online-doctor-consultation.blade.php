4abb9069335e6a19e893ca1c6f0b1628b66887b5
krishna <krishna.fnatic@gmail.com>
2020-06-15 14:45:03 +0530

    fix

--- resources/views/take-online-doctor-consultation.blade.php
+++ resources/views/take-online-doctor-consultation.blade.php
@@ -1,6 +1,29 @@
-@extends('layouts.master')
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="noindex, nofollow" />
 
-@section('content')
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@300;500&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" href="assets/css/opd.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <title>Take An Online Doctor Consultation Now!</title>
+  </head>
+  <body>
+    <header>
+      <div class="fluid-container">
+        <div class="row m-0">
+          <div class="col text-left pl-0"> </div>
+          <div class="col text-center"> <a href="" class="logo"> only<span>OPD</span> </a> </div>
+          <div class="col text-right pr-0">  </div>
+        </div>
+      </div>
+    </header>
+
 <div class="container">
   
   <div class="row mt-4">
@@ -540,8 +563,8 @@
     </div>
   </div>
 </div>
-@endsection
-@section('footer')
+
+
 <footer class="text-center p-2">
       <div class="container"> <a onclick="$('#terms').modal({
             backdrop: 'static',
@@ -551,4 +574,20 @@
             keyboard: false
         });"> Privacy </a> | &nbsp; 2020 &copy; Copyrights onlyOPD </div>
     </footer>
-@endsection
+    <script
+        src="https://code.jquery.com/jquery-3.4.1.js"
+        integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="{{asset('assets/js/opd-custom.js')}}"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-167493010-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-167493010-1');
+    </script>
+  </body>
+</html>
