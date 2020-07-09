6890e8f54e31d89313350fb61256f7923e84cc64
krishna <krishna.fnatic@gmail.com>
2020-07-09 19:31:08 +0530

    fix

--- resources/views/enter_otp.blade.php
+++ resources/views/enter_otp.blade.php
@@ -41,7 +41,7 @@
 
 @section('footer')
 <footer class="text-center p-2">
-      <div class="container"> <a href="/terms/index.html" target="_blank"> Terms </a> | <a href="/privacy/index.html" target="_blank"> Privacy </a> | &nbsp; 2020 &copy; Copyrights onlyOPD </div>
+      <div class="container"> <a href="/terms" target="_blank"> Terms </a> | <a href="/privacy" target="_blank"> Privacy </a> | &nbsp; 2020 &copy; Copyrights onlyOPD </div>
     </footer>
 <script type="text/javascript">
 
